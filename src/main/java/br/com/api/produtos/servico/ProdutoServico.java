331bf2c100d7f65621d9e2a7cf24fe68fd8999a8
Wellynton Spagnol <wellyntonspagnol10@gmail.com>
2023-02-25 15:12:50 -0300

    Sabadera

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -23,25 +23,28 @@ public class ProdutoServico {
   }
 
   // Método para cadastrar ou alterar produto
-  public ResponseEntity<?> cadastrarAndAlterar(ProdutoModelo pm,String acao) {
-
+  public ResponseEntity<?> cadastrarAndAlterar(ProdutoModelo pm, String acao) {
     if (pm.getNome().equals("")) {
-      rm.setMensagem("O nome do prosuto é obrigatório!");
+      rm.setMensagem("O nome do produto é obrigatório!");
       return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
     } else if (pm.getMarca().equals("")) {
       rm.setMensagem("O nome da marca é obrigatório!");
       return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
     } else {
-        if(acao.equals("cadastrar")){
-          return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
-        }else{
-      return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
-        }
+      if (acao.equals("cadastrar")) {
+        return new ResponseEntity<ProdutoModelo>(
+          pr.save(pm),
+          HttpStatus.CREATED
+        );
+      } else {
+        return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+      }
     }
   }
+  //Commit sabado
 
   // Método para remover produtos
-  public ResponseEntity<RespostaModelo> remover(Long codigo){
+  public ResponseEntity<RespostaModelo> remover(Long codigo) {
     pr.deleteById(codigo);
     rm.setMensagem("O produto foi removido com sucesso");
     return new ResponseEntity<>(rm, HttpStatus.OK);
